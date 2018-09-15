c7190587e3197efc959056a35a1d1dbe799f3af2
none Frontier <liuyiquancov5@gmail.com>
2018-09-15 14:12:28 +0800

    upload some components

--- src/components/home.vue
+++ src/components/home.vue
@@ -51,6 +51,7 @@
 			</div>
 
 			<div class="clear"></div>
+
 		</div>
 		<b class="line"></b>
 		<div class="shopNav">
@@ -79,133 +80,29 @@
 						<i class="am-icon-angle-right" style="padding-left: 10px;"></i>
 					</div>
 				</div>
-				<div class="bannerTwo">
-					<!--轮播 -->
-					<div class="am-slider am-slider-default scoll" data-am-flexslider id="demo-slider-0">
-						<ul class="am-slides">
-							<li class="banner1">
-								<a href="introduction.html"><img src="../images/ad5.jpg" /></a>
-							</li>
-							<li class="banner2">
-								<a><img src="../images/ad6.jpg" /></a>
-							</li>
-							<li class="banner3">
-								<a><img src="../images/ad7.jpg" /></a>
-							</li>
-							<li class="banner4">
-								<a><img src="../images/ad8.jpg" /></a>
-							</li>
-						</ul>
-					</div>
-					<div class="clear"></div>
-				</div>
-									<div id="nav" class="navfull" style="position: static;">
-					<div class="area clearfix">
-						<div class="category-content" id="guide_2">
-							<div class="category" style="box-shadow:none ;margin-top: 2px;">
-								<ul class="category-list navTwo" id="js_climit_li">
-									<li>
-										<div class="category-info">
-										  <h3 class="category-name b-category-name"><i><img src="../images/cake.png"></i><a class="ml-22" title="点心">点心/蛋糕</a></h3>
-													<em>&gt;</em>
-										</div>
-										<div class="menu-item menu-in top">
-											<div class="area-in">
-												<div class="area-bg">
-													<div class="menu-srot">
-														<div class="sort-side">
-															<dl class="dl-sort">
-																<dt><span title="蛋糕">蛋糕</span></dt>
-																<dd><a title="蒸蛋糕" href="#"><span>蒸蛋糕</span></a></dd>
-																<dd><a title="脱水蛋糕" href="#"><span>脱水蛋糕</span></a></dd>
-																<dd><a title="瑞士卷" href="#"><span>瑞士卷</span></a></dd>
-																<dd><a title="软面包" href="#"><span>软面包</span></a></dd>
-																<dd><a title="马卡龙" href="#"><span>马卡龙</span></a></dd>
-																<dd><a title="千层饼" href="#"><span>千层饼</span></a></dd>
-																<dd><a title="甜甜圈" href="#"><span>甜甜圈</span></a></dd>
-																<dd><a title="蒸三明治" href="#"><span>蒸三明治</span></a></dd>
-																<dd><a title="铜锣烧" href="#"><span>铜锣烧</span></a></dd>
-															</dl>
-															<dl class="dl-sort">
-																<dt><span title="蛋糕">点心</span></dt>
-																		<dd><a title="蒸蛋糕" href="#"><span>蒸蛋糕</span></a></dd>
-																		<dd><a title="脱水蛋糕" href="#"><span>脱水蛋糕</span></a></dd>
-																		<dd><a title="瑞士卷" href="#"><span>瑞士卷</span></a></dd>
-																		<dd><a title="软面包" href="#"><span>软面包</span></a></dd>
-																		<dd><a title="马卡龙" href="#"><span>马卡龙</span></a></dd>
-																		<dd><a title="千层饼" href="#"><span>千层饼</span></a></dd>
-																		<dd><a title="甜甜圈" href="#"><span>甜甜圈</span></a></dd>
-																		<dd><a title="蒸三明治" href="#"><span>蒸三明治</span></a></dd>
-																		<dd><a title="铜锣烧" href="#"><span>铜锣烧</span></a></dd>
-															</dl>
-															
-														</div>
-														<div class="brand-side">
-															<dl class="dl-sort">
-																<dt><span>实力商家</span></dt>
-																<dd><a rel="nofollow" title="呵官方旗舰店" target="_blank" href="#"><span  class="red" >呵官方旗舰店</span></a></dd>
-															</dl>
-														</div>
-													
-													
-													</div>
-												</div>
-											</div>
-										</div>
-									</li>
-									<li>
-										<div class="category-info">
-										  <h3 class="category-name b-category-name"><i><img src="../images/cake.png"></i><a class="ml-22" title="点心">点心/蛋糕</a></h3>
-													<em>&gt;</em>
-										</div>
-									</li>
-									<li>
-										<div class="category-info">
-										  <h3 class="category-name b-category-name"><i><img src="../images/cake.png"></i><a class="ml-22" title="点心">点心/蛋糕</a></h3>
-													<em>&gt;</em>
-										</div>
-									</li>
-									<li>
-										<div class="category-info">
-										  <h3 class="category-name b-category-name"><i><img src="../images/cake.png"></i><a class="ml-22" title="点心">点心/蛋糕</a></h3>
-													<em>&gt;</em>
-										</div>
-									</li>
-									<li>
-										<div class="category-info">
-										  <h3 class="category-name b-category-name"><i><img src="../images/cake.png"></i><a class="ml-22" title="点心">点心/蛋糕</a></h3>
-													<em>&gt;</em>
-										</div>
-									</li>
-									<li>
-										<div class="category-info">
-										  <h3 class="category-name b-category-name"><i><img src="../images/cake.png"></i><a class="ml-22" title="点心">点心/蛋糕</a></h3>
-													<em>&gt;</em>
-										</div>
-									</li>
-									<li>
-										<div class="category-info">
-										  <h3 class="category-name b-category-name"><i><img src="../images/cake.png"></i><a class="ml-22" title="点心">点心/蛋糕</a></h3>
-													<em>&gt;</em>
-										</div>
-									</li>
-									
-									
-								
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-
-			
-			
+				<Banner></Banner>
+				<Menu></Menu>
 			</div>
 		</div>
-	</div>
+
+</div>
 	
 </template>
-
+<script>
+	import Banner from '../components/banner'
+	import Menu from '../components/menu'
+	export default {
+  data () {
+      return {
+          items: []   /* 定义一个空数组数据items */
+      }
+  },
+  components: {
+    Banner,
+    Menu
+  }
+}
+</script>
 
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/home'
-
+import banner from '@/components/banner'
+import menu from '@/components/menu'
 Vue.use(Router)
 
 export default new Router({
@@ -11,6 +12,16 @@ export default new Router({
       name: 'Home',
       component: Home
     },
+    {
+      path: '/',
+      name: 'banner',
+      component: banner
+    },
+    {
+      path:'/',
+      name:'menu',
+      component: Home
+    }
   ]
 })
 
