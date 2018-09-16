cd806c9783cb02761b58100c27ae4147862d7a6d
none Frontier <liuyiquancov5@gmail.com>
2018-09-16 20:02:47 +0800

    optimize header template

--- src/components/header.vue
+++ src/components/header.vue
@@ -32,6 +32,22 @@
 					</div>
 				</ul>
 			</div>
+
+			<!--search-->
+			<div class="nav white">
+				<div class="logo"><img src="../images/logo.png" /></div>
+				<div class="logoBig">
+					<li><img src="../images/logobig.png" /></li>
+				</div>
+
+				<div class="search-bar pr">
+					<a name="index_none_header_sysc" href="#"></a>
+					<form>
+						<input id="searchInput" name="index_none_header_sysc" type="text" placeholder="搜索" autocomplete="off">
+						<input id="ai-topsearch" class="submit am-btn" value="搜索" index="1" type="submit">
+					</form>
+				</div>
+			</div>
 	</div>
 </template>
 <script>
