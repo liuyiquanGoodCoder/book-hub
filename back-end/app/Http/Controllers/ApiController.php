9479634ecc908590689fee400d91c8617b65fb7b
Balerion <15982361670@126.com>
2018-09-25 22:26:02 +0800

    invalid 501 change

--- back-end/app/Http/Controllers/ApiController.php
+++ back-end/app/Http/Controllers/ApiController.php
@@ -1,17 +1,17 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
 use App\Http\Requests\RegisterAuthRequest;
 use App\User;
 use Illuminate\Http\Request;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
- 
+
 class ApiController extends Controller
 {
     public $loginAfterSignUp = true;
- 
+
     public function register(RegisterAuthRequest $request)
     {
         $user = new User();
@@ -19,44 +19,44 @@ class ApiController extends Controller
         $user->email = $request->email;
         $user->password = bcrypt($request->password);
         $user->save();
- 
+
         if ($this->loginAfterSignUp) {
             return $this->login($request);
         }
- 
+
         return response()->json([
             'success' => true,
             'data' => $user
         ], 200);
     }
- 
+
     public function login(Request $request)
     {
         $input = $request->only('email', 'password');
         $jwt_token = null;
- 
+
         if (!$jwt_token = JWTAuth::attempt($input)) {
             return response()->json([
                 'success' => false,
                 'message' => 'Invalid Email or Password',
-            ], 401);
+            ]);
         }
- 
+
         return response()->json([
             'success' => true,
             'token' => $jwt_token,
         ]);
     }
- 
+
     public function logout(Request $request)
     {
         $this->validate($request, [
             'token' => 'required'
         ]);
- 
+
         try {
             JWTAuth::invalidate($request->token);
- 
+
             return response()->json([
                 'success' => true,
                 'message' => 'User logged out successfully'
@@ -68,15 +68,15 @@ class ApiController extends Controller
             ], 500);
         }
     }
- 
+
     public function getAuthUser(Request $request)
     {
         $this->validate($request, [
             'token' => 'required'
         ]);
- 
+
         $user = JWTAuth::authenticate($request->token);
- 
+
         return response()->json(['user' => $user]);
     }
 }
