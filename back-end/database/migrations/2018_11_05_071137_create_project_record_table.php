ada2674f42081c8b9f288ff85c5309ccc5b49f3c
SunLongxu <sunlong1218@gmail.com>
2018-11-05 15:14:56 +0800

    "Added project_record table"

---
+++ back-end/database/migrations/2018_11_05_071137_create_project_record_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectRecordTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_record', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')
+                ->unsigned();
+            $table->integer('from_seller_id')
+                ->unsigned();
+            $table->string('details');
+            $table->string('price');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_record');
+    }
+}
