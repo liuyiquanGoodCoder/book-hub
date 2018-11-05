cc402ebac770d7e0afa32af1e36bb80cd1d2e1bb
SunLongxu <sunlong1218@gmail.com>
2018-11-05 14:23:59 +0800

    "Updated user-table role comment"

---
+++ back-end/database/migrations/2018_11_05_061459_update_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('role')
+                ->comment('buyer,0;seller,1;finance,2;marketing,3;hr,4;customerservice,5;')
+                ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
