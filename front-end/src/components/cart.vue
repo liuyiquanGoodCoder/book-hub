ac27f80e88e8a10f7602f254842dfe0054e0c4ec
none Frontier <liuyiquancov5@gmail.com>
2018-11-12 16:42:17 +0800

    Add dialog

--- front-end/src/components/cart.vue
+++ front-end/src/components/cart.vue
@@ -5,21 +5,70 @@
 			div.cart-row
 				div.block-header
 					h4 My Cart
-				div.order-lst-panel
+				div.order-list-panel
 					div.order-panel
+						table.cart-table
+							tr
+								td Book Name
+								td Price
+								td.format Format
+								td.quantity Quantity
+							tr
+								td.book-name
+									a Head First JavaScript
+								td 109
+								td.format mobi
+								td.quantity
+									input(value="1")
+					div.address.order-operation
+						h5 Address
+						div.address-content
+							div.pull-right
+								a.form-link(@click="vm.showDialog=true") change
+
+							div.default-address
+								table.cart-table
+									tr.thead
+										td Name
+										td Region
+										td Information
+										td Phone Number
+									tr
+										td liu
+										td China
+										td Taiyuan
+										td 93115197
+					div.order.order-operation
+						h5 Order
+		Dialog(@closeDialog="close" v-if="vm.showDialog")
+						
+
 </template>
 <script>
 	import Header from '../components/header'
+	import Dialog from '../components/dialog'
 	export default {
 		data () {
 			return {
-				items: []   /* 定义一个空数组数据items */
+				 vm: {
+	              showDialog: false,
+	            } 
 			}
 		},
 		components: {
-			Header
-		}
+			Header,
+			Dialog
+		},
+		methods:{
+			dialog(){
+				this.isShow = true;	
+			},
+			close(){
+	          	this.vm.showDialog = false;
+	        }
 	}
+}
+
 </script>
 <style lang="scss">
 .container{
@@ -37,6 +86,81 @@
 			    margin: 30px 0 6px 0;
 			    border-bottom: 1px solid $Greyunderline;
 			    position: relative;
+			    h4{
+			    	font-size: 24px;
+			    }
+	    	}
+	    	.order-list-panel{
+	    		zoom: 1;
+	    		.order-panel{
+	    			box-sizing: border-box;
+	    			.cart-table{
+	    				width: 100%;
+	    				border-collapse: collapse;
+	    				border: 1px solid $Greyunderline;
+	    				tr{
+	    					border: 1px solid $Greyunderline;
+	    					td{
+	    						height: 40px;
+    							line-height: 40px;			
+	    					}
+	    					.format{
+	    						width: 10%;
+	    					}
+	    					.quantity{
+	    						width: 10%;
+	    						text-align: center;
+	    					}
+	    					input{
+	    						display: inline-block;
+	    						text-align: center;
+	    						width: 15px;
+	    						height: 15px;
+	    						line-height: 15px;
+	    						outline: none;
+	    					}
+	    				}
+	    				
+	    			}
+	    			
+	    		}
+	    		.order-operation{
+	    			position: relative;
+	    				h5{
+	    					font-size: 17px;
+	    					background: none;
+						    border: none;
+						    padding: 0;
+						    height: auto;
+						    margin: 30px 0 6px 0;
+						    border-bottom: 1px solid $Greyunderline;	
+	    				}
+	    		}
+	    		.address{
+	    			.pull-right{
+	    				float: right;
+	    				.form-link{
+	    					color: $titleBlue;
+	    				}
+	    			}
+	    			.default-address{
+	    				 box-sizing: border-box;
+	    				.cart-table{
+	    					 width: 100%;
+	    					 border: 1px solid $Greyunderline;
+	    					 border-collapse: collapse;
+	    					.thead{
+	    						background:$backgroundGrey;
+	    					}
+	    					tr{
+	    						td{
+	    							height: 40px;
+    								line-height: 40px;
+	    						}
+	    					}
+	    				}
+	    			}
+	    		}
 	    	}
 
 	    }
---
+++ front-end/src/components/dialog.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="dialog">
+    <div class="border">
+      <div @click="closeDialog" class="closeImg"></div>
+      <span class="message">这是一段信息</span>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data(){
+      return {}
+    },
+    methods:{
+      closeDialog(){
+         //给父组件传参
+         this.$emit('closeDialog',false)
+      }
+    }
+  }
+</script>
+ 
+<style scoped>
+  #dialog{
+    position: fixed;
+    top: 0;
+    left: 0;
+    background: rgba(0,0,0,0.3);
+    width: 100%;
+    height: 100%;
+  }
+  .message{
+    text-align: center;
+    position: absolute;
+    top: 50%;
+  }
+  .closeImg{
+    width: 20px;
+    height: 20px;
+    float: right;
+    margin-right: 5px;
+    margin-top: 5px;
+    cursor: pointer;
+    background-image: url();
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+  .border{
+    text-align: center;
+    background-color: white;
+    border-radius: 20px;
+    width: 50%;
+    height: 50%;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+  }
+</style>
