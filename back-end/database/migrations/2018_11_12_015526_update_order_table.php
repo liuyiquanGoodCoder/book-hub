a5a64a7e00af5caa555926445f607094b18b62bf
SunLongxu <sunlong1218@gmail.com>
2018-11-12 10:01:59 +0800

    "updated db orders oid to address_id"

---
+++ back-end/database/migrations/2018_11_12_015526_update_order_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('orders', function (Blueprint $table) {
+            $table->renameColumn('oid', 'address_id');
+            $table->foreign('address_id')
+                ->references('id')
+                ->on('addresses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
