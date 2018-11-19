f194bf6b772d2b0ec08167ea08bffd27feaa64ae
SunLongxu <sunlong1218@gmail.com>
2018-11-19 13:17:51 +0800

    "Added some comments for routes"

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -21,22 +21,28 @@ Route::post('login', 'ApiController@login')->middleware('cors:api');
 Route::post('register', 'ApiController@register')->middleware('cors:api');
 
 Route::middleware('auth.jwt', 'cors:api')->group (function() {
+	//all: userinfo management
 	Route::get('user', 'ApiController@getAuthUser');
 	Route::get('logout', 'ApiController@logout');
  
+	//buyer: carts management
 	Route::post('addtocarts', 'CartsController@addtocarts');
 	Route::get('showcarts', 'CartsController@showcarts');
 
+	//buyer: address management
 	Route::get('showaddresses', 'AddressesController@showaddresses');
 	Route::post('addaddresses', 'AddressesController@addaddresses');
 	
+	//buyer: order management
 	Route::post('orderbooks', 'CartsController@orderbooks');
 	Route::get('showorders', 'CartsController@showorders');
 
+	//seller: store management
 	Route::post('create_storeinfo', 'StoreController@create_storeinfo');
 	Route::get('show_storeinfo', 'StoreController@show_storeinfo');
 	Route::post('update_storeinfo', 'StoreController@update_storeinfo');
 
+	//finance: project management
 	Route::post('create_projects', 'ProjectController@create_projects');
 	Route::get('show_projects', 'ProjectController@show_projects');
 	Route::post('update_projects', 'ProjectController@update_projects');
@@ -56,6 +62,7 @@ Route::middleware('auth.jwt', 'cors:api')->group (function() {
 });
 
 Route::middleware('cors:api')->group (function() {
+	//buyer-unlogin: search and show
 	Route::get('home', 'BooksController@home');
 	Route::get('findbook/{bname}', 'BooksController@findbook');
 	Route::post('bookdet', 'BooksController@bookdet');
