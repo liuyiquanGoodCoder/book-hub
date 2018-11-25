b527dcba44babd859ece7b11f2603eb8c6781bdd
none Frontier <liuyiquancov5@gmail.com>
2018-11-25 11:15:13 +0800

    finsih register

--- front-end/src/components/newregister.vue
+++ front-end/src/components/newregister.vue
@@ -7,17 +7,60 @@
 			div.message Use Your Book Hub 
 			div.user_information
 				div.user_title Email Address
-				input.user_email
+				input.user_email(v-model="userName")
 				div.underline
 				div.user_title Password
 				div.user_title Confirm Password
-				input.user_password(type="password")
+				input.user_password(type="password",v-model="password")
 				input.user_confrim_password(type="password")
 				div.user_confirm
 					router-link(to="/newlogin",class="register") Sign in
-					div.login Create
+					div.login(@click="register") Create
 </template>
 <script>
+	import Header from '../components/header'
+	export default{
+		data(){
+			return {
+				title:true,
+				error:"",
+				userName:"",
+				password:"",
+				
+			}
+		},
+		components: {
+
+		},
+		methods: {
+			register(){
+				const $this = this;
+				const url = "http://jwt.test/api/register";
+				axios.post(url, {
+					name:$this.userName,
+				    email: $this.userName,
+				    password: $this.password,
+				    role:0
+				  })
+				  .then(function (response) {
+				    console.log(response);
+				    if(response.data.success == false){
+				    	//
+				    }else if(response.data.success == true){
+				    	var userInfo = {};
+				    		userInfo.userName = $this.userName.substring(0, $this.userName.indexOf('@'));
+				    	    userInfo.token = response.data.token;
+				    	document.cookie = name + '=;  expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+				    	document.cookie = userInfo.userName +'|' +userInfo.token;
+				    	$this.$router.go(-1);
+				    }
+				  })
+				  .catch(function (error) {
+				    console.log(error);
+				});
+			}
+		}
+	}
 </script>
 <style lang="scss" scoped>
 	.login_container{
