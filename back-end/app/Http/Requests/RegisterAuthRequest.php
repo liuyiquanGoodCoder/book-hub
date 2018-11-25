f667f3a02a4ea87ba0a4a5b184475a0ac456d862
SunLongxu <sunlong1218@gmail.com>
2018-11-25 10:02:02 +0800

    "Fixed register bugs"

--- back-end/app/Http/Requests/RegisterAuthRequest.php
+++ back-end/app/Http/Requests/RegisterAuthRequest.php
@@ -26,7 +26,8 @@ class RegisterAuthRequest extends FormRequest
         return [
             'name' => 'required|string',
             'email' => 'required|email|unique:users',
-            'password' => 'required|string|min:6|max:10'
+            'password' => 'required|string|min:6|max:10',
+            'role' => 'required'
         ];
     }
 }
