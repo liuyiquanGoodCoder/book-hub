fc056a3e0cf17ea96b3c603757180312b94edb54
SunLongxu <sunlong1218@gmail.com>
2018-11-26 18:25:58 +0800

    "Created performances table"

---
+++ back-end/database/migrations/2018_11_26_045837_create_performances_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePerformancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('performances', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')
+                ->unsigned();
+            $table->integer('ability');
+            $table->integer('effective');
+            $table->integer('accurate');
+            $table->integer('communication');
+            $table->integer('efficient');
+            $table->integer('policy');
+            $table->string('totalscore');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('performances');
+    }
+}
