85ab26da3e73f6288b37ceeb818ddde88dd90bb8
none Frontier <liuyiquancov5@gmail.com>
2018-11-29 17:03:36 +0800

    upgrade sidebar template

--- back-end/resources/views/home.blade.php
+++ back-end/resources/views/home.blade.php
@@ -2,11 +2,41 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Larvuent</title>
+     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>My BookHub</title>
+
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 <body>
-    <div id="app"></div>
+    <div id="app" class="hold-transition sidebar-mini"></div>
 
     <script src="{{ mix('js/app.js') }}"></script>
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.js"></script>
+
+    <!-- OPTIONAL SCRIPTS -->
+    <script src="dist/js/demo.js"></script>
+
+    <!-- PAGE PLUGINS -->
+    <!-- SparkLine -->
+    <script src="plugins/sparkline/jquery.sparkline.min.js"></script>
+    <!-- jVectorMap -->
+    <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+    <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <!-- SlimScroll 1.3.0 -->
+    <script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
+    <!-- ChartJS 1.0.2 -->
+    <script src="plugins/chartjs-old/Chart.min.js"></script>
 </body>
 </html>
