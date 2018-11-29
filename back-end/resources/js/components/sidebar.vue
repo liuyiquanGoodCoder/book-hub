11dee7ea43d7119727d0115188d4cc9ebdcf0d54
none Frontier <liuyiquancov5@gmail.com>
2018-11-29 18:22:58 +0800

    finish sidebar

--- back-end/resources/js/components/sidebar.vue
+++ back-end/resources/js/components/sidebar.vue
@@ -1,4 +1,124 @@
 <template>
+    <div>
+        <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="index3.html" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Contact</a>
+      </li>
+    </ul>
+
+    <!-- SEARCH FORM -->
+    <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fa fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Messages Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="fa fa-comments-o"></i>
+          <span class="badge badge-danger navbar-badge">3</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Brad Diesel
+                  <span class="float-right text-sm text-danger"><i class="fa fa-star"></i></span>
+                </h3>
+                <p class="text-sm">Call me whenever you can...</p>
+                <p class="text-sm text-muted"><i class="fa fa-clock-o mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  John Pierce
+                  <span class="float-right text-sm text-muted"><i class="fa fa-star"></i></span>
+                </h3>
+                <p class="text-sm">I got your message bro</p>
+                <p class="text-sm text-muted"><i class="fa fa-clock-o mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Nora Silvester
+                  <span class="float-right text-sm text-warning"><i class="fa fa-star"></i></span>
+                </h3>
+                <p class="text-sm">The subject goes here</p>
+                <p class="text-sm text-muted"><i class="fa fa-clock-o mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+        </div>
+      </li>
+      <!-- Notifications Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="fa fa-bell-o"></i>
+          <span class="badge badge-warning navbar-badge">15</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <span class="dropdown-header">15 Notifications</span>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fa fa-envelope mr-2"></i> 4 new messages
+            <span class="float-right text-muted text-sm">3 mins</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fa fa-users mr-2"></i> 8 friend requests
+            <span class="float-right text-muted text-sm">12 hours</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fa fa-file mr-2"></i> 3 new reports
+            <span class="float-right text-muted text-sm">2 days</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i
+            class="fa fa-th-large"></i></a>
+      </li>
+    </ul>
+  </nav>
            <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
@@ -12,10 +132,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img v-bind:src="userImage" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{userName}}</a>
         </div>
       </div>
 
@@ -25,15 +145,8 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                Marketing
-                <i class="right fa fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview" style="display: none;">
+          <li class="nav-item has-treeview" v-if="seller">
+            <ul>
                <li class="nav-item">
                 <a href="/storeinfo" class="nav-link">
                   <i class="nav-icon fa fa-th"></i>
@@ -51,47 +164,19 @@
                 </p>
               </a>
             </li>
-               <li class="nav-item">
-                <a href="/neworders" class="nav-link">
-                  <i class="nav-icon fa fa-th"></i>
-                  <p>
-                    New Orders
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="/expenditure" class="nav-link">
-                  <i class="nav-icon fa fa-th"></i>
-                  <p>
-                    Expenditure Records
-                  </p>
-                </a>
-              </li>
-            </ul>
-          </li>
-
-           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                Accounting
-                <i class="right fa fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview" style="display: none;">
-              <li class="nav-item">
+            <li class="nav-item">
                 <a href="/myservices" class="nav-link">
                   <i class="nav-icon fa fa-th"></i>
                   <p>
                     Services
                   </p>
                 </a>
-              </li> 
-              <li class="nav-item">
-                <a href="/income" class="nav-link">
+              </li>
+               <li class="nav-item">
+                <a href="/neworders" class="nav-link">
                   <i class="nav-icon fa fa-th"></i>
                   <p>
-                    Income Records
+                    New Orders
                   </p>
                 </a>
               </li>
@@ -103,14 +188,37 @@
                   </p>
                 </a>
               </li>
+              
+            </ul>
+          </li>
+
+           <li class="nav-item" v-if="finance">
+            <ul>
+              
               <li class="nav-item">
-                <a href="/projects" class="nav-link">
+                <a href="/income" class="nav-link">
                   <i class="nav-icon fa fa-th"></i>
                   <p>
-                    Marketing Projects
+                    Income Records
                   </p>
                 </a>
-              </li> 
+              </li>
+              <li class="nav-item">
+                <a href="/expenditure" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Expenditure Records
+                  </p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="/mybusiness" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Business
+                  </p>
+                </a>
+              </li>
                <li class="nav-item">
                 <a href="/payroll" class="nav-link">
                   <i class="nav-icon fa fa-th"></i>
@@ -122,28 +230,53 @@
             </ul>
           </li>
 
-           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                Human Resource
-                <i class="right fa fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview" style="display: none;">
+           <li class="nav-item" v-if="marking">
+            <ul>
               <li class="nav-item">
-                <a href="/employeeinfo" class="nav-link">
+                <a href="/stores" class="nav-link">
                   <i class="nav-icon fa fa-th"></i>
                   <p>
-                    Employee
+                    Store management
                   </p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="/resumes" class="nav-link">
+                <a href="/projects" class="nav-link">
                   <i class="nav-icon fa fa-th"></i>
                   <p>
-                    Resumes
+                    Projects Management
+                  </p>
+                </a>
+              </li>
+               <li class="nav-item">
+                <a href="/projectrecords" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Project Records
+                  </p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="/business" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Business
+                  </p>
+                </a>
+              </li>
+              
+              
+            </ul>
+          </li>
+
+          <li class="nav-item" v-if="human">
+            <ul>
+              
+              <li class="nav-item">
+                <a href="/employeeinfo" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Employee Information
                   </p>
                 </a>
               </li>
@@ -155,21 +288,14 @@
                   </p>
                 </a>
               </li>
-              
-            </ul>
-          </li>
-
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                Business
-                <i class="right fa fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview" style="display: none;">
-              
-              
+              <li class="nav-item">
+                <a href="/resumes" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Resumes
+                  </p>
+                </a>
+              </li>
             </ul>
           </li>      
         </ul>
@@ -179,18 +305,49 @@
     </div>
     <!-- /.sidebar -->
   </aside>
+  </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            
+            userName:"",
+            seller:false,
+            finance:false,
+            marking:false,
+            human:false,
+            userImage:""
         }
     },
     mounted(){
-        
+        let userInfo = document.cookie.split(";");
+        //let token = userInfo[0].XSRF-TOKEN;
+        let userFunction = userInfo[0].split("|");
+        let role = userFunction[1];
+        this.userName = userFunction[0];
+        switch (role){
+            case "1":
+                this.seller = true;
+                this.userImage="dist/img/user1-128x128.jpg"
+                break;
+            case "2": 
+                this.finance = true;
+                this.userImage="dist/img/user2-160x160.jpg"
+                break;
+            case "3": 
+                this.marking = true;
+                this.userImage="dist/img/user3-128x128.jpg"
+                break;
+            case "4": 
+                this.human = true;
+                this.userImage="dist/img/user4-128x128.jpg"
+                break;
+
+
+        }
     }
+
 }
 </script>
 
