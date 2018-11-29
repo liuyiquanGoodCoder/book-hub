0d8b3e3820fa731b73c5666afff8c52d63b08c51
none Frontier <liuyiquancov5@gmail.com>
2018-11-29 12:42:07 +0800

    finish sidebar

--- back-end/resources/views/Seller/mybusiness.blade.php
+++ back-end/resources/views/Seller/mybusiness.blade.php
@@ -165,126 +165,161 @@ scratch. This page gets rid of all links and provides the needed markup only.
       </div>
 
       <!-- Sidebar Menu -->
+           <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item">
-            <a href="/storeinfo" class="nav-link">
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
               <i class="nav-icon fa fa-th"></i>
               <p>
-              Store Management
+                Marketing
+                <i class="right fa fa-angle-left"></i>
               </p>
             </a>
+            <ul class="nav nav-treeview" style="display: none;">
+               <li class="nav-item">
+                <a href="/storeinfo" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                  Store Management
+                  </p>
+                </a>
+              </li>
+
+             <li class="nav-item">
+              <a href="/booksinfo" class="nav-link">
+                <i class="nav-icon fa fa-th"></i>
+                <p>
+                Books Management
+                </p>
+              </a>
+            </li>
+               <li class="nav-item">
+                <a href="/neworders" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    New Orders
+                  </p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="/expenditure" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Expenditure Records
+                  </p>
+                </a>
+              </li>
+            </ul>
           </li>
-          <li class="nav-item">
-            <a href="/booksinfo" class="nav-link">
+
+           <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
               <i class="nav-icon fa fa-th"></i>
               <p>
-              Books Management
+                Accounting
+                <i class="right fa fa-angle-left"></i>
               </p>
             </a>
+            <ul class="nav nav-treeview" style="display: none;">
+              <li class="nav-item">
+                <a href="/myservices" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Services
+                  </p>
+                </a>
+              </li> 
+              <li class="nav-item">
+                <a href="/income" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Income Records
+                  </p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="/mybusiness" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Business
+                  </p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="/projects" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Marketing Projects
+                  </p>
+                </a>
+              </li> 
+               <li class="nav-item">
+                <a href="/payroll" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Payroll
+                  </p>
+                </a>
+              </li>
+            </ul>
           </li>
-          <li class="nav-item">
-            <a href="/myservices" class="nav-link">
+
+           <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
               <i class="nav-icon fa fa-th"></i>
               <p>
-                My Services
+                Human Resource
+                <i class="right fa fa-angle-left"></i>
               </p>
             </a>
+            <ul class="nav nav-treeview" style="display: none;">
+              <li class="nav-item">
+                <a href="/employeeinfo" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Employee
+                  </p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="/resumes" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Resumes
+                  </p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="/training" class="nav-link">
+                  <i class="nav-icon fa fa-th"></i>
+                  <p>
+                    Training
+                  </p>
+                </a>
+              </li>
+              
+            </ul>
           </li>
-          <li class="nav-item">
-            <a href="/mybusiness" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                My Business
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/neworders" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                New Orders
-                <span class="right badge badge-danger">New</span>
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/employeeinfo" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                Employee Management
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/resumes" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                New Resumes
-                <span class="right badge badge-danger">New</span>
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/training" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                Training Projects
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/stores" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                Store Management
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/projects" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                Marketing Projects
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/business" class="nav-link">
+
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
               <i class="nav-icon fa fa-th"></i>
               <p>
                 Business
+                <i class="right fa fa-angle-left"></i>
               </p>
             </a>
-          </li>
-          <li class="nav-item">
-            <a href="/payroll" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                Payroll
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/expenditure" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                Expenditure Records
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/income" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                Income Records
-              </p>
-            </a>
-          </li>
+            <ul class="nav nav-treeview" style="display: none;">
+              
+              
+            </ul>
+          </li>      
         </ul>
       </nav>
+  
       <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
